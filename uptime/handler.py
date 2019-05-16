c230f7a0db3fe403a1c792d46be7584677561b51
Alex Ellis <alexellis2@gmail.com>
2019-05-16 14:58:33 +0100

    Update handler.py

--- uptime/handler.py
+++ uptime/handler.py
@@ -3,5 +3,5 @@ import requests
 def handle(req):
     r = requests.get(req)
     if r.status_code == 200 or r.status_code == 301 or r.status_code == 303:
-        return "up"
-    return "down"
+        return "up (" +str(r.status_code)+ ")"
+    return "down (" +str(r.status_code)+ ")"
